11a7b685832695cdab727e70c15b5bad317d53c8
Cesar Borrego <Cesar Borrego@CesarBorrego.csumb.edu>
2023-03-06 19:36:56 -0800

    Added AssignmentDTO

---
+++ cst438_gradebook/src/main/java/com/cst438/domain/AssignmentDTO.java
@@ -0,0 +1,14 @@
+package com.cst438.domain;
+
+public class AssignmentDTO {
+
+	public AssignmentDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
